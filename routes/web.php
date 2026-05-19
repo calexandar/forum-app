0042a708c78e59868c802a5945f9a3d56cc6c82d
calexandar <alexandercvet243@gmail.com>
2026-05-19 12:10:23 +0200

    feat: store post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
@@ -16,3 +17,6 @@ require __DIR__.'/settings.php';
 
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+
+Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('posts.comments.store');
---
+++ tests/Feature/Controllers/CommentController/StoreTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+
+it('can store a comment', function () {
+   $user = User::factory()->create();
+   $post = Post::factory()->create();
+
+   $response = $this->actingAs($user)->post(route('posts.comments.store', $post), [
+       'body' => 'This is a comment',
+   ]);
+
+   $this->assertDatabaseHas(Comment::class, [
+       'user_id' => $user->id,
+       'post_id' => $post->id,
+       'body' => 'This is a comment',
+   ]);
+});
+
+it('redirects after storing a comment', function () {
+     $user = User::factory()->create();
+    $post = Post::factory()->create();
+
+    $response = $this->actingAs($user)
+        ->post(route('posts.comments.store', $post), [
+            'body' => 'This is a comment',
+    ]);
+
+    $response->assertRedirect(route('posts.show', $post));
+});
+
+it('requires validation', function ($value) {
+    $post = Post::factory()->create();
+
+    $response = $this->actingAs(User::factory()->create())
+        ->post(route('posts.comments.store', $post), [
+            'body' => $value,
+    ]);
+
+    $response->assertInvalid(['body']);
+})->with([
+    null,
+    '',
+    true,
+    123,
+    str_repeat('a', 1001),
+]);
