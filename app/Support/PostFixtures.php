74262dbb32904be0fec38475995d6f19a2453a5a
calexandar <alexandercvet243@gmail.com>
2026-06-10 10:27:08 +0200

    fix(postFixtures): aded once removed static

--- app/Support/PostFixtures.php
+++ app/Support/PostFixtures.php
@@ -8,17 +8,16 @@ use Illuminate\Support\Facades\File;
 
 class PostFixtures
 {
-    private static Collection $fixtures;
 
     public static function getFixtures(): Collection
     {
-        return self::$fixtures ??= collect(File::files(database_path('factories/fixtures/posts')))
+        return once(fn () => collect(File::files(database_path('factories/fixtures/posts')))
             ->map(fn (SplFileInfo $file) => $file->getContents())
             ->map(fn (string $content) => str($content)->explode("\n", 2))
             ->map((fn (Collection $parts) => [
                 'title' => str($parts->first())->trim()->after('#'),
                 'body' => str($parts->last())->trim(),
-            ]));
+            ])));
 
     }
 }
